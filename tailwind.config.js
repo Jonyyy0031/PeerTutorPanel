6152414ffe5be896b1e81a2c8b0db8aa0086d658
jony <jaxelhm16@gmail.com>
2025-05-28 14:57:38 -0500

    update fondo source

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,21 @@ module.exports = withMT({
   theme: {
     extend: {
       backgroundImage: {
-        UniBg: "url('./src/assets/FondoLogi.svg')",
+        UniBg: "url('/FondoLogi.svg')",
       },
       keyframes: {
-        'slide-in': {
-          '0%': { transform: 'translateX(100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
+        "slide-in": {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
         },
-        'slide-out': {
-          '0%': { transform: 'translateX(0)', opacity: '1' },
-          '100%': { transform: 'translateX(100%)', opacity: '0' },
+        "slide-out": {
+          "0%": { transform: "translateX(0)", opacity: "1" },
+          "100%": { transform: "translateX(100%)", opacity: "0" },
         },
       },
       animation: {
-        'slide-in': 'slide-in 0.3s ease-out',
-        'slide-out': 'slide-out 0.3s ease-in',
+        "slide-in": "slide-in 0.3s ease-out",
+        "slide-out": "slide-out 0.3s ease-in",
       },
       colors: {
         primary: {
